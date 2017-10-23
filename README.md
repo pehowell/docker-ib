5091eb0418186d46eba1433e7b3b64a0ae8182ce
Paul Howell <paul.howell@gmail.com>
2017-10-23 11:54:00 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Headless Interactive Brokers Gateway Docker image
+
+A stripped down Arch Linux Docker image running the IB-Controller and TWS AUR packages.
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euxo pipefail
+
+ln -s config ${USERID}
+
+/usr/sbin/xvfb-run --auto-servernum -f /var/run/xvfb/ibtws /usr/share/ib-tws/jre/bin/java \
+-cp /usr/share/ib-tws/jars/*:/usr/share/java/ibcontroller/ibcontroller.jar -Xmx512M ibcontroller.IBGatewayController config/config.ini
